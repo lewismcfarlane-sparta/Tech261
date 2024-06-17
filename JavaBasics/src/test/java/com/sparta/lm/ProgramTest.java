9cc2e313558bac2e0bcfe8626f4e02f1ac77f958
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-06-17 15:10:03 +0100

    Added JUnit and created a simple test

---
+++ JavaBasics/src/test/java/com/sparta/lm/ProgramTest.java
@@ -0,0 +1,12 @@
+package com.sparta.lm;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProgramTest {
+
+    @Test
+    public void basicTest() {
+        Assertions.assertTrue(5 == 3);
+    }
+}
