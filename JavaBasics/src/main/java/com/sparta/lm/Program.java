8038a7e1287ec7a5fc8a2e455c959b120c50b717
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-06-17 16:29:23 +0100

    fix: correct time range conditons and and handling for invalid time in getGreeting
    - Change getGreeting method from private to public for testing access.
    - Fix condition for "Good morning!" to be exclusive of the upper bound.
    - Fix condition for "Good afternoon!" to be exclusive of the upper bound.
    - Add condition for "Good evening!" with to cover the time frame 18 to 22, exclusive with the upper bound.
    - Add a new condition for "Goodnight!" to cover the time range from 22 to 24 and 0 to 5.
    - Amend the else clause to handle unexpected values.

--- JavaBasics/src/main/java/com/sparta/lm/Program.java
+++ JavaBasics/src/main/java/com/sparta/lm/Program.java
@@ -6,15 +6,20 @@ public class Program {
         System.out.println(getGreeting(timeOfDay));
     }
 
-    private static String getGreeting(int timeOfDay) {
+    public static String getGreeting(int timeOfDay) {
         String greeting;
-        if (timeOfDay >= 5 && timeOfDay <= 12) {
+        if (timeOfDay >= 5 && timeOfDay < 12) {
             greeting = "Good morning!";
-        } else if (timeOfDay >= 12 && timeOfDay <= 18) {
+        } else if (timeOfDay >= 12 && timeOfDay < 18) {
             greeting = "Good afternoon!";
-        } else {
+        } else if (timeOfDay >= 18 && timeOfDay < 22) {
             greeting = "Good evening!";
+        } else if ((timeOfDay >= 22 && timeOfDay < 24) || (timeOfDay >= 0 && timeOfDay < 5 )) {
+            greeting = "Goodnight!";
+        } else {
+            greeting = "Invalid time of day!";
         }
+
         return greeting;
     }
 }
