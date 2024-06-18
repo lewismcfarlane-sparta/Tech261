605762eb54561fd3a3a257f7aaeb43153ed877c7
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-06-18 11:37:55 +0100

    refactor: Change to nested if else loop

--- JavaBasics/src/main/java/com/sparta/lm/FizzBuzz.java
+++ JavaBasics/src/main/java/com/sparta/lm/FizzBuzz.java
@@ -3,14 +3,12 @@ package com.sparta.lm;
 public class FizzBuzz {
     public static String getFizzBuzzFrom(int number) {
         String fizzBuzzString = Integer.toString(number);
-        if (number % 3 == 0) {
-            fizzBuzzString = "Fizz";
-        }
-        if (number % 5 == 0) {
-            fizzBuzzString = "Buzz";
-        }
         if (number % 5 == 0 && number % 3 == 0) {
             fizzBuzzString = "FizzBuzz";
+        } else if (number % 3 == 0) {
+            fizzBuzzString = "Fizz";
+        } else if (number % 5 == 0) {
+            fizzBuzzString = "Buzz";
         }
         if (number == 0) {
             fizzBuzzString = "0";
