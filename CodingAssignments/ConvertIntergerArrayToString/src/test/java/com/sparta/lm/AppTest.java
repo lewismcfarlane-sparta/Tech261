9e998ea967ea41c9ce644c41cc98a2d7d8536aca
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-06-18 17:47:46 +0100

    test: Build test initial test for array conversion

---
+++ CodingAssignments/ConvertIntergerArrayToString/src/test/java/com/sparta/lm/AppTest.java
@@ -0,0 +1,31 @@
+package com.sparta.lm;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+//Given an array of integers, convert them to a single int made up of the digits
+//
+//for example the array:
+//
+//int[] numbers = {1, 0, 2, 4}
+//
+//should return the int 1024.
+
+public class AppTest {
+    @Test
+    @DisplayName("Given multiple integers in an array, return the combined integers as a single integer")
+    void checkMultipleIntegerConversion() {
+        int expected = 1234;
+        int[] array = {1, 2, 3, 4};
+        int actual = App.covertArrayToInteger(array);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Given a single integer in an array, return it as a single integer")
+    void checkSingleIntegerConversion() {
+
+    }
+}
