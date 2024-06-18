9d499ee69e423e53272848c849126b8b2b3adf61
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-06-18 15:42:02 +0100

    Added project for Abstract Classes and Interfaces

---
+++ AbstractClassesAndInterfaces/src/main/java/com/sparta/lm/Dog.java
@@ -0,0 +1,43 @@
+package com.sparta.lm;
+
+public class Dog {
+    private String name;
+    private String breed;
+    private int age;
+
+    public Dog(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        //int age = 5, type, name, value. Same for constructor
+        Dog max = new Dog("Max", "Terrier", 2);
+        System.out.println(max.getBreed());
+    }
+}
