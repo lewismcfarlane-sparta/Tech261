bfe976f44cfd9bf87af4f8f86d4953e2d25263b8
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-06-19 17:20:35 +0100

    test: Build tests for functionality

--- CodingAssignments/IdenticalNeighbours/src/test/java/com/sparta/lm/AppTest.java
+++ CodingAssignments/IdenticalNeighbours/src/test/java/com/sparta/lm/AppTest.java
@@ -6,13 +6,59 @@ import org.junit.jupiter.api.Test;
 
 public class AppTest {
     @Test
-    @DisplayName("Test")
+    @DisplayName("Check if returns false when array is null")
     void checkIfInputArrayIsNull() {
         int[] input = null;
         boolean expected = false;
 
-        boolean actual = App.checkIfAdjacentIndicesAreIdentical(input);
+        boolean actual = App.hasThreeIdenticalNeighbouringIndices(input);
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    @DisplayName("Three adjacent identical neighbours in the middle")
+    void checkIfInputArrayFindsThreeAdjacentIdenticalNumbersInTheMiddle() {
+        int[] input = {0, 1, 1, 4, 4, 4, 5};
+        boolean expected = true;
+        boolean actual = App.hasThreeIdenticalNeighbouringIndices(input);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Three adjacent identical neighbours at the start")
+    void checkIfInputArrayFindsThreeAdjacentIdenticalNumbersAtTheStart() {
+        int[] input = {1, 1, 1, 0, 4, 4, 5};
+        boolean expected = true;
+        boolean actual = App.hasThreeIdenticalNeighbouringIndices(input);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Three adjacent identical neighbours at the end")
+    void checkIfInputArrayFindsThreeAdjacentIdenticalNumbersAtTheEnd() {
+        int[] input = {0, 1, 1, 3, 4, 4, 4};
+        boolean expected = true;
+        boolean actual = App.hasThreeIdenticalNeighbouringIndices(input);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Arrays smaller than three returns false")
+    void checkIfReturnsFalseForArraysSmallerThanThree() {
+        int[] input = {1, 1};
+        boolean expected = false;
+        boolean actual = App.hasThreeIdenticalNeighbouringIndices(input);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Empty arrays")
+    void checkIfReturnsFalseForEmptyArrays() {
+        int[] input = {1, 1};
+        boolean expected = false;
+        boolean actual = App.hasThreeIdenticalNeighbouringIndices(input);
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
