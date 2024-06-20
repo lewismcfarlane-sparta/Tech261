ba400056e210e754666dc649254bd99637dd883e
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-06-20 13:03:20 +0100

    strings example code

--- Strings/src/main/java/com/sparta/lm/StringExamples.java
+++ Strings/src/main/java/com/sparta/lm/StringExamples.java
@@ -2,6 +2,16 @@ package com.sparta.lm;
 
 public class StringExamples {
     public static void main(String[] args) {
+        String string1 = "Hello"; // Strings are
+        string1.toUpperCase(); // Immutable
+        string1 = string1.toUpperCase();
+//        System.out.println(string1);
+//
+//        System.out.println(string1.toCharArray());
 
+        String string = String.valueOf(100);
+
+        String name = "Manish";
+        System.out.println("Hello " + name);
     }
 }
