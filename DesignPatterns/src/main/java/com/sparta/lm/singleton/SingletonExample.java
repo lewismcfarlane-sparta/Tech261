ad7cceb48629307f2df366a5a5f67cd402bc3f0b
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-06-25 16:02:15 +0100

    Add singleton example

--- DesignPatterns/src/main/java/com/sparta/lm/singleton/SingletonExample.java
+++ DesignPatterns/src/main/java/com/sparta/lm/singleton/SingletonExample.java
@@ -1,4 +1,11 @@
 package com.sparta.lm.singleton;
 
 public class SingletonExample {
+    private static final SingletonExample SINGLETON_EXAMPLE = new SingletonExample();
+    private SingletonExample(){}
+
+    public static SingletonExample getSingletonExample() {
+        return SINGLETON_EXAMPLE;
+    }
+
 }
